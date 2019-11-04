41a90432cbe65435343dac52ac69c241ad1746b7
luancaius <luancaius@outlook.com>
2019-11-03 20:27:56 -0500

    feat: updating timing

--- camera/example2.py
+++ camera/example2.py
@@ -6,7 +6,7 @@ camera = picamera.PiCamera()
 interval=30
 continueCapture = True
 start=datetime(2019, 11, 3,19,44)
-end=datetime(2019, 11, 3,19,52)
+end=datetime(2019, 11, 3,20,10)
 timeStart = start.timestamp()
 timeEnd = end.timestamp()
 
@@ -16,13 +16,13 @@ while continueCapture:
     now=datetime.now()
     index+=1
     print('video ',index,' - ', now)
-    camera.start_recording('video{}.mp4'.format(index))
+    camera.start_recording('videos/video{}.h264'.format(index))
     time.sleep(interval)
     camera.stop_recording()    
     timestamp = now.timestamp()
     diffStart = timestamp-timeStart
     diffEnd = timeEnd-timestamp
-    print(diffStart," | ", diffEnd)
+    
     if diffStart > 0 and diffEnd > 0:
         continueCapture = True
     else:
